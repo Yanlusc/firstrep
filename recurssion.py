e497efc73863a7e38efeef5821d41718425a56b7
YannickLusci <yan.e.live@live.be>
2021-01-12 19:32:56 +0100

    uncommited files

---
+++ recurssion.py
@@ -0,0 +1,11 @@
+n=int(input('entrer le nombre n'))
+
+def rec(n):
+    if n < 1:
+        return 1
+    result= n * rec(n-1)
+    print(str(n) + "! = " + str(result))
+    return result
+
+
+print(rec(n))<
