aa82b1cfad17e6f519d713c0afa6a39da8b65db0
YannickLusci <yan.e.live@live.be>
2021-01-13 20:48:31 +0100

    update eighth

---
+++ 2-%placeholder.py
@@ -0,0 +1,10 @@
+# a = 'Jean Jacques a 45 ans et a 3 enfants'
+# b = 'Marie a 32 ans et a 5 enfants'
+
+# print(a, b, sep='\n')
+
+def afficheFamille(personne, age, nb_enfants):
+    return '%s a %d ans et a %s enfants' % (personne, age, nb_enfants)
+
+print(afficheFamille('Jean Jacques',45,3))
+print(afficheFamille('Marie',32,5))
